9a03b697d95e87087d5658ed615f875249dacc6e
tanmay1965 <tanmaychalla@gmail.com>
2024-05-29 22:05:54 +0530

    Create Caluculator.py

---
+++ Caluculator.py
@@ -0,0 +1,26 @@
+a=int(input('Enter a Value'))
+b=int(input('Enter a Value'))
+print('1.+')
+print('2.-')
+print("3.*")
+print('4./')
+print('5.%')
+print('6.**')
+print('7.//')
+c=input('select your option')
+if c=='+':
+	print(a+b)
+elif c=='-':
+	print(a-b)
+elif c=='*':
+	print(a*b)
+elif c=='/':
+	print(a/b)
+elif c=='%':
+	print(a%b)
+elif c=='**'
+	print(a**b)
+elif c=='//':
+	print(a//b)
+else :
+	print('Wrong option')
